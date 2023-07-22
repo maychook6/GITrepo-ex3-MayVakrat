1d36576d083b773df666dd7e43661619a194fb39
May Vakrat <mvakrat6@gmail.com>
2023-07-22 23:26:00 +0300

    trigger1

---
+++ MayVakrat.java
@@ -0,0 +1,7 @@
+public class MayVakrat {
+    public static void main(String[] args) { 
+        for (int i=1;i<=10; i++) { 
+            System.out.println("May Vakrat ..."+i);  
+        }
+    }   
+}
